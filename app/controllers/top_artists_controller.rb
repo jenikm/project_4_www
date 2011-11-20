6cbe27cd11e690c8c528b6a8f9d64c9ccb4e1249
Camillo Lugaresi <camillo.lugaresi@gmail.com>
2011-11-20 00:40:36 -0600

    filter top artists by gender

--- app/controllers/top_artists_controller.rb
+++ app/controllers/top_artists_controller.rb
@@ -6,6 +6,20 @@ select id, mbid, name, plays
 from artists
 join (select artist_id, sum(plays) as plays
 from top_artists
+EOS
+		where = []
+		if params[:gender]
+			where << "gender = 'm'" if params[:gender].index('m')
+			where << "gender = 'f'" if params[:gender].index('f')
+			where << "gender is null" if params[:gender].index('u')
+		end
+		
+		if where.length > 0
+			query << <<-EOS
+where #{where.join(' or ')}
+EOS
+		end
+		query << <<-EOS
 group by artist_id
 order by plays desc
 limit 10) ta
